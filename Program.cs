b3c7b1e27cd3e0e7e289611b4f754648fc536fe6
Chiranjivi Jawale <cj@wunderfund.co>
2019-02-15 00:16:50 -0500

    Adding FindAnswerGame

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int randomNumber1 = randomNumber.Next(2, 11);
+            int randomNumber2 = randomNumber.Next(2, 11);
+            int answer, computedAnswer;
+
+
+
+            Console.Write("What is the answer for " + randomNumber1 + " * " + randomNumber2 + "? \n");
+            answer = Convert.ToInt32(Console.ReadLine());
+            computedAnswer = (randomNumber1 * randomNumber2);
+            if (answer == computedAnswer)
+            {
+                Console.Write("You are right!");
+            }
+            else
+            {
+                Console.Write("The correct answer is: " + computedAnswer);
+            }
+
+        }
+    }
+}
